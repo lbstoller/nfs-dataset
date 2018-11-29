3a18582b8b4869bc76a8d997f189b8cef66cdceb
Leigh Stoller <lbstoller@gmail.com>
2018-11-29 12:28:13 -0800

    Update profile.py

--- profile.py
+++ profile.py
@@ -56,7 +56,7 @@ nfsServer.disk_image = params.osImage
 # Attach server to lan.
 nfsLan.addInterface(nfsServer.addInterface())
 # Initialization script for the server
-nfsServer.addService(pg.Execute(shell="sh", command="sudo /proj/testbed/stoller/nfs-server.sh"))
+nfsServer.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repository/nfs-server.sh"))
 
 # Special node that represents the ISCSI device where the dataset resides
 dsnode = request.RemoteBlockstore("dsnode", nfsDirectory)
@@ -77,7 +77,7 @@ for i in range(1, params.clientCount+1):
     node.disk_image = params.osImage
     nfsLan.addInterface(node.addInterface())
     # Initialization script for the clients
-    node.addService(pg.Execute(shell="sh", command="sudo /proj/testbed/stoller/nfs-client.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repository/nfs-client.sh"))
     pass
 
 # Print the RSpec to the enclosing page.
